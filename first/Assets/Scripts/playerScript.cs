283aaf7930283dc0632332f80b953b002cd8bc64
geronto <tobbergur@gmail.com>
2018-11-16 22:08:15 +0000

    Added jumping

--- first/Assets/Scripts/playerScript.cs
+++ first/Assets/Scripts/playerScript.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class playerScript : MonoBehaviour {
 
 	public float moveSpeed;
+	private Rigidbody rb;
+	public float jumpForce = 7;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 dirVector = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")).normalized;
- 		GetComponent <Rigidbody> ().MovePosition (transform.position + dirVector * Time.deltaTime * moveSpeed);
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//Movement
+		Vector3 dirVector = new Vector3 (moveHorizontal, 0, moveVertical).normalized;
+ 		rb.MovePosition (transform.position + dirVector * Time.deltaTime * moveSpeed);
+
+		// Jumping
+		bool isGrounded = rb.transform.position.y <= 0.5;
+		
+		if(isGrounded && Input.GetKeyDown(KeyCode.Space)){
+			rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+		}
 	}
 }
