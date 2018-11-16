e93277724c0a216b44844f8f992c33b2e05499c1
geronto <tobbergur@gmail.com>
2018-11-16 20:04:30 +0000

    Character movement

---
+++ first/Assets/Scripts/playerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dirVector = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")).normalized;
+ 		GetComponent <Rigidbody> ().MovePosition (transform.position + dirVector * Time.deltaTime * moveSpeed);
+	}
+}
